267812bba5a42b0f416c512f7a8015919004985f
Leandro Gomez <leandro.gomez73@protonmail.com>
2021-04-19 19:04:14 -0500

    Fix linting

--- website/gunicorn.py
+++ website/gunicorn.py
@@ -10,10 +10,10 @@ def max_workers():
 
 
 def app_port():
-    return os.environ.get('PORT', '8000')
+    return os.environ.get("PORT", "8000")
 
 
-bind = '0.0.0.0:%s' % app_port()
+bind = "0.0.0.0:%s" % app_port()
 
 workers = max_workers()
 
@@ -22,16 +22,16 @@ max_requests = 5000
 max_requests_jitter = 500
 
 raw_env = [
-    "DJANGO_SETTINGS_MODULE=%s" % os.environ.get('DJANGO_SETTINGS_MODULE', 'website.settings.production'),
+    "DJANGO_SETTINGS_MODULE=%s" % os.environ.get("DJANGO_SETTINGS_MODULE", "website.settings.production"),
 ]
 
 timeout = 400
 
 # https://docs.gunicorn.org/en/19.9.0/settings.html#errorlog
 # Equivalent to --log-file
-errorlog = os.environ.get('ERROR_LOG', '-')
+errorlog = os.environ.get("ERROR_LOG", "-")
 
-accesslog = os.environ.get('ACCESS_LOG', '-')
+accesslog = os.environ.get("ACCESS_LOG", "-")
 
 # See https://aws.amazon.com/premiumsupport/knowledge-center/elb-capture-client-ip-addresses/
 # https://docs.aws.amazon.com/elasticloadbalancing/latest/userguide/how-elastic-load-balancing-works.html#http-headers
