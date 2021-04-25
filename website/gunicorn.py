452cd1f63b7d1a09c60b46072e97dbf5ca9e0bf1
Leandro Gomez <leandro.gomez73@protonmail.com>
2021-04-25 17:26:59 -0500

    Add gevent

--- website/gunicorn.py
+++ website/gunicorn.py
@@ -13,6 +13,10 @@ def app_port():
     return os.environ.get("PORT", "8000")
 
 
+worker_class = "gevent"
+
+proc_name = "django-base-gunicorn"
+
 bind = "0.0.0.0:%s" % app_port()
 
 workers = max_workers()
@@ -32,23 +36,6 @@ timeout = 400
 errorlog = os.environ.get("ERROR_LOG", "-")
 
 accesslog = os.environ.get("ACCESS_LOG", "-")
-
-# See https://aws.amazon.com/premiumsupport/knowledge-center/elb-capture-client-ip-addresses/
-# https://docs.aws.amazon.com/elasticloadbalancing/latest/userguide/how-elastic-load-balancing-works.html#http-headers
-
-# %({x-forwarded-for}i)s : Remote ip forwarded by AWS Loadbalancer
-# %(h)s : remote address. Actually this is a private ip when using a AWS LoadBalancer
-# %(l)s : "-" string
-# %(t)s : date of the request
-# %(s)s : status
-# "%(r)s" : status line (e.g. GET / HTTP/1.1)
-# %(q)s : Query string
-# %(b)s : response length or '-' (CLF format)
-# %(T)s :  request time in seconds
-# %(f)s :  referrer
-# %(a)s : user agent
-# %({x-hw-build-number}i)s : X-HW-BUILD-NUMBER if present
-access_log_format = '%({x-forwarded-for}i)s %(h)s %(l)s %(t)s %(s)s "%(r)s" "%(q)s" %(b)s %(T)s "%(f)s" "%(a)s"'
 
 app_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
