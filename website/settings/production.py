51656ae4d3f9b8c197c13efd9fa53bef164d57d5
Leandro Gomez <leandro.gomez73@protonmail.com>
2021-04-25 15:12:30 -0500

    Add static conf and whitenoise

--- website/settings/production.py
+++ website/settings/production.py
@@ -1,1 +1,8 @@
 from .base import *
+
+PROD_MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # https://warehouse.python.org/project/whitenoise/
+]
+MIDDLEWARE = MIDDLEWARE + PROD_MIDDLEWARE
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
