4997243bbbb03e05978f4166c397e974a823b782
Leandro Gomez <leandro.gomez73@protonmail.com>
2021-04-25 15:51:21 -0500

    Add file cache to prod

--- website/settings/production.py
+++ website/settings/production.py
@@ -10,3 +10,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ALLOWED_HOSTS = [
     "django-base-7337.herokuapp.com",
 ]
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, "django_default_cache"),
+    }
+}
